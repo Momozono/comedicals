1522b268c3a9985716377795f06fc5a48518e69d
Momozono <tanatana1089@gmail.com>
2015-07-07 15:23:17 +0900

    finish signin/signout system

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
   def create
   	@user = User.new(user_params)
   	if @user.save
+  		sign_in @user
   		flash[:success] = "Welcome to the Fragment!"
   		redirect_to @user
   	else
