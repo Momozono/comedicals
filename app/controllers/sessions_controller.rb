8242897551c89db2cb73257cb17b2e9ae39a072f
Momozono <tanatana1089@gmail.com>
2015-07-08 09:43:47 +0900

    7/8 morning

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,7 @@ class SessionsController < ApplicationController
 
 	def create
 		user = User.find_by(email: params[:session][:email].downcase)
-		if user && user.authenticate(params[:session][:password])
+		if user && user.authenticate(params[:session][:password]) #authenticate成功でuserデータが帰ってくる
 			sign_in user
 			redirect_to user
 		else
--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -13,16 +13,12 @@ module SessionsHelper
 		@current_user = user
 	end
 	def current_user
-		remember_token = User.encrypt(cookies[:remember_token])
-		@current_user ||= User.find_by(remember_token: remember_token)
+		remember_token = User.encrypt(cookies[:remember_token]) #クッキーに格納された記憶トークンを暗号化
+		@current_user ||= User.find_by(remember_token: remember_token) #Userモデルから記憶トークンをもとに検索
 	end
 
 	def sign_out
 		self.current_user = nil
 		cookies.delete(:remember_token)
 	end
-
-
-
 end
-
