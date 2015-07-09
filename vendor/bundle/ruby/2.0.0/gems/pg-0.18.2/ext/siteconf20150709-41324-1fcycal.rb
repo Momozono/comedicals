9975961e61f57c431ad772cd545363f591c35407
Junya Namatame <momozono@Junya-no-iMac.local>
2015-07-09 16:25:57 +0900

    Initial commit for iMac

--- vendor/bundle/ruby/2.0.0/gems/pg-0.18.2/ext/siteconf20150709-41324-1fcycal.rb
+++ vendor/bundle/ruby/2.0.0/gems/pg-0.18.2/ext/siteconf20150709-41324-1fcycal.rb
@@ -1,5 +1,5 @@
 require 'rbconfig'
-dest_path = "./.gem.20150705-16003-yajinx"
+dest_path = "./.gem.20150709-41324-yoyc08"
 RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
 RbConfig::CONFIG['sitearchdir'] = dest_path
 RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
