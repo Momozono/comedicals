c66c1de8d31852e56f4bf88e10f39a8540872480
Junya Namatame <momozono@Junya-no-iMac.local>
2015-07-13 12:43:17 +0900

    delete profession column

--- README.md
+++
@@ -1,4 +0,0 @@
-*****README*****
-
-Original SNS  which write code fragment & comment.
-Write Casually more than other webapp.
--- spec/factories.rb
+++
@@ -1,8 +0,0 @@
-FactoryGirl.define do 
-	factory :user do
-		name                  "Momozono sann"
-		email                 "foobarhoge@gmail.com"
-		password              "foobar"
-		password_confirmation "foobar"
-	end
-end
