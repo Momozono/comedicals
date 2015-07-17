3e5f9b7a57f7ec493c8a15341417504843f7bb06
Junya Namatame <momozono@Junya-no-iMac.local>
2015-07-17 10:32:17 +0900

    create profession modlel test

--- app/models/profession.rb
+++ app/models/profession.rb
@@ -1,4 +1,5 @@
 class Profession < ActiveRecord::Base
   has_many :user_professions
   has_many :users, through: :user_professions
+  validates :profession_name, presence: true
 end
--- spec/models/profession_spec.rb
+++ spec/models/profession_spec.rb
@@ -1,9 +1,16 @@
 require 'spec_helper'
-=begin
 
 describe Profession do
+
   before do
-    @profession = Profession.new()
+    @profession = Profession.new(profession_name: "doctor")
+  end
+  it "should have the right atttribute" do
+    expect(@profession).to respond_to(:profession_name)
+  end
+
+  it "should not be blank profession_name" do
+    @profession.profession_name = nil
+    expect(@profession).to be_invalid
   end
 end
-=end
