4735906a813861f683dcc2a3baa491543a655b5b
Junya Namatame <momozono@Junya-no-iMac.local>
2015-07-17 17:06:44 +0900

    bugfix when signed_out,undefind method

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,13 +1,15 @@
 class StaticPagesController < ApplicationController
   def home
-    @micropost = current_user.microposts.build if signed_in?
-    @feed_items = current_user.feed
-    @professions = current_user.professions
+    if signed_in?
+      @micropost = current_user.microposts.build
+      @feed_items  = current_user.feed
+      @professions = current_user.professions
+    end
   end
 
   def help
   end
 
-  def abou
+  def about
   end
 end
