21f37f61ce70022fdc8852908ccab154b17a6eb0
Junya Namatame <momozono@Junya-no-iMac.local>
2015-07-21 15:49:00 +0900

    create delete function in user feed

--- app/controllers/microposts_controller.rb
+++ app/controllers/microposts_controller.rb
@@ -1,5 +1,6 @@
 class MicropostsController < ApplicationController
-  before_action :signed_in_user
+  before_action :signed_in_user, only: [:create, :destroy]
+  before_action :correct_user, only: :destroy
 
   def create
     @micropost = current_user.microposts.build(micropost_params)
@@ -16,10 +17,17 @@ class MicropostsController < ApplicationController
   end
 
   def destroy
+    @micropost.destroy
+    redirect_to root_url
   end
 
     private
       def micropost_params
         params.require(:micropost).permit(:content)
+      end
+
+      def correct_user
+        @micropost = current_user.microposts.find_by(id: params[:id])
+        redirect_to root_url if @micropost.nil?
       end
 end
